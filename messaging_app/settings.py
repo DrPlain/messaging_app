1033a9e60b8830f88d237ef6a9def60a2895464a
Gideon Obiasor <gideonoiasor@gmail.com>
2025-01-27 23:32:36 +0100

    Add github actions to setup, connect to mysql and run tests

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PRODUCTION = False
+PRODUCTION = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=lambda v: [
                        s.strip() for s in v.split(',')])
